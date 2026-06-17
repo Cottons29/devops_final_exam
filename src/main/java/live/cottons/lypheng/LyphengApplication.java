280061edfcc1ab82d2ccef932422a35840cd089d
Cottons29 <lycuttons@gmail.com>
2026-06-17 15:12:58 +0700

    Initialize Lypheng Spring Boot application with Gradle build and model definitions

---
+++ src/main/java/live/cottons/lypheng/LyphengApplication.java
@@ -0,0 +1,13 @@
+package live.cottons.lypheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LyphengApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LyphengApplication.class, args);
+	}
+
+}
---
+++ src/test/java/live/cottons/lypheng/LyphengApplicationTests.java
@@ -0,0 +1,13 @@
+package live.cottons.lypheng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LyphengApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
